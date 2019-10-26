1c1f4787f7329597d99af7eebd8535e6054c1f4a
zhanxinl <zhanxinl@iu.edu>
2019-10-26 14:00:35 -0400

    update

--- bechmark_llr.R
+++ bechmark_llr.R
@@ -4,7 +4,7 @@ library(bench)
 #setwd("..")
 #dir<-getwd()
 #setwd(toString(paste(dir,"/Stat610/",sep='')))
-setwd("/Users/addytan/Stat610/")
+setwd("/Users/zhangxinlu/dev/HW5/")
 
 bnchmark <- bench::mark(
   source("llr_functions.R"))
