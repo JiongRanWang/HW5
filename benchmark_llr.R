cdd4abe3043c5e77e907e00fbd262bbc9a36d28d
jiowang <jiowang@iu.edu>
2019-10-28 14:42:17 -0400

    change file

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -4,7 +4,7 @@ library(bench)
 #setwd("..")
 #dir<-getwd()
 #setwd(toString(paste(dir,"/Stat610/",sep='')))
-setwd("/Users/jiongranwang/Desktop/S610")
+#setwd("/Users/jiongranwang/Desktop/S610")
 
 bnchmark <- bench::mark(
   source("llr_functions.R"))
