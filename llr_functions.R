0214d7e6bd2b66b649dc9ec0743310730f969120
jiowang <jiowang@iu.edu>
2019-10-28 14:40:45 -0400

    change file

--- llr_functions.R
+++ llr_functions.R
@@ -23,8 +23,11 @@ llr = function(x, y, z, omega) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = make_weight_matrix(z, x, omega)
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  #f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  #f_hat = c(1, z) %*% solve(t(X) %*% apply(t(X),1,function(x){Wz*x})) %*% t(X) %*% (Wz * y)
+  f_hat = c(1, z) %*% solve(t(X)%*% sweep(X, 1 , Wz, FUN = "*")) %*% t(X) %*% (Wz*y)
   return(f_hat)
+  
 }
 
 #' @param z (numeric) must be a scalar
@@ -34,7 +37,8 @@ compute_f_hat = function(z, x, y, omega) {
 make_weight_matrix = function(z, x, omega) {
   r = abs(x - z) / omega  # this is a vector of the same length as x
   w = sapply(r, W)  # this is a vector of the same length as x and r
-  Wz = diag(w)  # this is a diagonal matrix with elements from w
+  #Wz = diag(w)  # this is a diagonal matrix with elements from w
+  Wz = w
   return(Wz)
 }
 
